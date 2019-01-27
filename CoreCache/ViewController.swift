0e94ef107aecb9ecc3d11de6b2e1cf7135b57357
Josh Draper <mrjacoste@gmail.com>
2019-01-27 12:57:19 -0500

    Initial Commit

---
+++ CoreCache/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreCache
+//
+//  Created by Josh Draper on 1/27/19.
+//  Copyright © 2019 JDraper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
